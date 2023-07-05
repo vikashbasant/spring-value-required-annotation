8703a78dc6e4e1934c28714ce8d618cd2c6ab73a
Vikash Basant <basantvikash360@gmail.com>
2023-07-05 15:01:31 +0530

    we are using @Value() annotation

--- src/main/java/com/kash/Student.java
+++ src/main/java/com/kash/Student.java
@@ -1,18 +1,24 @@
 package com.kash;
 
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
+
 public class Student {
     private String name;
     private String interestedCourse;
     private String hobby;
 
+    @Value("${student.name}")
     public void setName(String name) {
         this.name = name;
     }
 
+    @Value("${student.interestedCourse}")
     public void setInterestedCourse(String interestedCourse) {
         this.interestedCourse = interestedCourse;
     }
 
+    @Value(value = "${student.hobby}")
     public void setHobby(String hobby) {
         this.hobby = hobby;
     }
