fde5d5483ef315a735c6a38fafb808fab3a8a434
Vikash Basant <basantvikash360@gmail.com>
2023-07-05 15:08:31 +0530

    Now we are using @Value() annotation without setter method

--- src/main/java/com/kash/Student.java
+++ src/main/java/com/kash/Student.java
@@ -4,27 +4,16 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 
 public class Student {
-    private String name;
-    private String interestedCourse;
-    private String hobby;
 
     @Value("${student.name}")
-    public void setName(String name) {
-        System.out.println("setter method for set name");
-        this.name = name;
-    }
+    private String name;
 
     @Value("${student.interestedCourse}")
-    public void setInterestedCourse(String interestedCourse) {
-        System.out.println("setter method for set interestedCourse");
-        this.interestedCourse = interestedCourse;
-    }
+    private String interestedCourse;
 
     @Value(value = "${student.hobby}")
-    public void setHobby(String hobby) {
-        System.out.println("setter method for set hobby");
-        this.hobby = hobby;
-    }
+    private String hobby;
+
 
     public void displayStudentInfo() {
         System.out.println("Student name: " + this.name);
